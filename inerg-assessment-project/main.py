5011a3665b44bed76c0d714a81d14e9ff288353e
abraham.jacob <abraham.jacob@innovaturelabs.com>
2024-11-27 15:21:03 +0530

    Fix: code formating and imports ordering
    Code formatted and imports sorted using black and isort

--- inerg-assessment-project/main.py
+++ inerg-assessment-project/main.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
-import click
-from flask.cli import with_appcontext
-from models import db, AnnualProductionData
-from dotenv import load_dotenv
 import os
+
+import click
+import pandas as pd
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
+
+from models import AnnualProductionData, db
 
 load_dotenv()
 
@@ -13,8 +15,8 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-    app.config['DEBUG'] = os.getenv('DEBUG')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
+    app.config["DEBUG"] = os.getenv("DEBUG")
 
     db.init_app(app)
     with app.app_context():
@@ -22,21 +24,23 @@ def create_app():
         print("Database and tables created successfully")
     return app
 
+
 app = create_app()
 
-@app.cli.command('load-data')
-@click.argument('file')
+
+@app.cli.command("load-data")
+@click.argument("file")
 # @click.option("--overwrite", is_flag=True, help="Set this flag to overwrite the existing data")
 @with_appcontext
 # def load_data(file, overwrite):
 def load_data(file):
     df = pd.read_excel(file)
     print("Columns", df.columns)
-    grouped_df = df.groupby('API WELL  NUMBER').agg({
-        'OIL': 'sum',
-        'GAS': 'sum',
-        'BRINE': 'sum'
-    }).reset_index()
+    grouped_df = (
+        df.groupby("API WELL  NUMBER")
+        .agg({"OIL": "sum", "GAS": "sum", "BRINE": "sum"})
+        .reset_index()
+    )
 
     # if overwrite:
     db.session.query(AnnualProductionData).delete()
@@ -44,28 +48,45 @@ def load_data(file):
     print("Existing data deleted. Inserting new data")
     new_entries = []
     for _, row in grouped_df.iterrows():
-        print("index", _, "API WELL NUMBER", row['API WELL  NUMBER'], "Oil", row['OIL'], "Gas", row['GAS'], "Brine", row['BRINE'])
-        new_data = AnnualProductionData(api_well_number=row['API WELL  NUMBER'].astype(str), oil=row['OIL'].astype(str), gas=row['GAS'].astype(str), brine=row['BRINE'].astype(str))
+        print(
+            "index",
+            _,
+            "API WELL NUMBER",
+            row["API WELL  NUMBER"],
+            "Oil",
+            row["OIL"],
+            "Gas",
+            row["GAS"],
+            "Brine",
+            row["BRINE"],
+        )
+        new_data = AnnualProductionData(
+            api_well_number=row["API WELL  NUMBER"].astype(str),
+            oil=row["OIL"].astype(str),
+            gas=row["GAS"].astype(str),
+            brine=row["BRINE"].astype(str),
+        )
         new_entries.append(new_data)
     db.session.bulk_save_objects(new_entries)
     db.session.commit()
     print("New data inserted successfully")
 
-@app.route('/data', methods=['GET'])
+
+@app.route("/data", methods=["GET"])
 def get_annual_data():
-    api_well_number = request.args.get('well')
+    api_well_number = request.args.get("well")
     if not api_well_number:
         return jsonify({"error": "Well number is required"}), 400
     print("api_well_number", api_well_number)
     data = AnnualProductionData.query.filter_by(api_well_number=api_well_number).first()
     print("data", data)
     if not data:
-        return jsonify({"error": f"No data found for API WELL NUMBER {api_well_number}"}), 404
-    return jsonify({
-        "oil": data.oil,
-        "gas": data.gas,
-        "brine": data.brine
-    })
+        return (
+            jsonify({"error": f"No data found for API WELL NUMBER {api_well_number}"}),
+            404,
+        )
+    return jsonify({"oil": data.oil, "gas": data.gas, "brine": data.brine})
+
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv('DEBUG'))
+    app.run(debug=os.getenv("DEBUG"))
--- inerg-assessment-project/models.py
+++ inerg-assessment-project/models.py
@@ -2,9 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class AnnualProductionData(db.Model):
 
-    __tablename__ = 'annual_production_data'
+    __tablename__ = "annual_production_data"
 
     id = db.Column(db.Integer, primary_key=True)
     api_well_number = db.Column(db.String(20), nullable=False)
